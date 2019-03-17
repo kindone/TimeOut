88ffb9d92973b77eed85c30979a578afefff22a6
Won-Wook Hong <jradoo@gmail.com>
2019-03-17 16:56:19 +0900

    basic working state
    * tray with elapsed time
    * timed and fade in/out
    * elapsed time
    * package

---
+++ src/index.ts
@@ -0,0 +1,120 @@
+import { app, BrowserWindow, dialog, ipcMain, Menu, Tray } from "electron";
+import * as moment from "moment";
+import * as path from "path";
+
+const debug = false
+
+const createMainWindow = () => {
+  // create browser window hidden
+  console.log("WINDOW CREATED")
+  const window = new BrowserWindow({
+    height: 600,
+    width: 800,
+    transparent: true,
+    title: "Simple Clock and Stop Watch",
+    alwaysOnTop: true,
+    frame: false,
+    closable: false,
+  })
+  window.loadURL(`file://${path.join(__dirname, "assets/index.html")}`)
+  window.maximize();
+  if (debug) {
+    window.webContents.openDevTools()
+  } else {
+    window.setIgnoreMouseEvents(true);
+  }
+
+  return window
+}
+
+
+class WindowHandle {
+  private window: BrowserWindow
+
+  constructor(isLazy = false) {
+    if (!isLazy)
+      this.window = createMainWindow()
+  }
+
+  public fadeIn() {
+    this.createIfNeeded()
+    this.window.show()
+    this.window.webContents.send("fadeIn")
+  }
+
+  public fadeOut() {
+    this.createIfNeeded()
+    this.window.webContents.send("fadeOut")
+    console.log("fadeOut")
+  }
+
+  public hide() {
+    this.createIfNeeded()
+    this.window.hide()
+    console.log("hide")
+  }
+
+  public close() {
+    if (this.window !== undefined && !this.window.isDestroyed()) {
+      this.window.destroy() // close() doesn't seem to work with closable=false
+    }
+  }
+
+  private createIfNeeded() {
+    if (this.window === undefined || this.window.isDestroyed()) {
+      this.window = createMainWindow()
+      this.window.on("closed", () => {
+        this.window = undefined
+      })
+    }
+  }
+
+}
+
+const startupMainWindow = () => {
+  return new WindowHandle()
+}
+
+const getNextSchedule = (): number =>  {
+  const now = moment()
+  const nextHour = moment(now).startOf("hour").add(1, "hours")
+  const next10Minutes = moment(now)
+    .set("minutes", now.minute() - (now.minute() % 10)).add(10, "minutes").startOf("minute")
+
+  const next = nextHour.isBefore(next10Minutes) ? nextHour : next10Minutes
+  const timeout = next.diff(now)
+  console.log(now, nextHour, next10Minutes, next, timeout)
+
+  return timeout
+}
+
+const startup = () => {
+  const showDuration = debug ? 6000 : 3000
+
+  const mainWindow = startupMainWindow()
+  // startupTray(mainWindow)
+
+  const hideAndScheduleShow = () => {
+    mainWindow.fadeOut()
+    setTimeout(showAndScheduleHide, getNextSchedule())
+  }
+
+  const showAndScheduleHide = () => {
+    mainWindow.fadeIn()
+    setTimeout(hideAndScheduleShow, showDuration)
+  }
+
+  showAndScheduleHide()
+
+  ipcMain.on("quit", () => {
+    mainWindow.close()
+    app.quit()
+  })
+
+  ipcMain.on("hide", () => {
+    mainWindow.hide()
+  })
+}
+
+app.dock.hide()
+app.on("ready", () => setTimeout(startup, 500));
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,90 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+import { dialog, ipcRenderer, Menu, remote } from "electron";
+import * as $ from "jquery"
+import * as moment from "moment";
+import * as path from "path";
+
+
+let stopWatch = moment()
+
+const fadeIn = () => {
+  console.log("fadeIn")
+  $(".wrapper").fadeIn("fast")
+}
+
+const fadeOut = () => {
+  console.log("fadeOut")
+  $(".wrapper").fadeOut("slow", () => {
+    console.log("hide")
+    ipcRenderer.send("hide")
+  })
+}
+
+const getElapsedTimeFormatted = (from: moment.Moment, now: moment.Moment) => {
+    const milliseconds = now.diff(from)
+    const normalized = Math.floor(milliseconds / (1000 * 60)) * 1000 * 60 // by minutes
+}
+
+
+const showTime = () => {
+    const now = moment()
+    const time = now.format("hh:mm:ss A")
+    document.getElementById("ClockDisplay").innerText = time;
+    document.getElementById("ClockDisplay").textContent = time;
+
+    let elapsed = ""
+    if (now.diff(stopWatch) > 1000 * 60 * 1) { // must be greater than some time to be useful
+        elapsed = now.from(stopWatch, true)
+    }
+    document.getElementById("StopWatchDisplay").innerText = elapsed;
+    document.getElementById("StopWatchDisplay").textContent = elapsed;
+    setTimeout(showTime, 1000);
+}
+
+showTime();
+
+const resetStopWatch = () => {
+    stopWatch = moment()
+}
+
+const startupTray = () => {
+    const assetsDirectory = path.join(__dirname, "assets")
+    const trayIcon = "timerTemplate.png" // must use *Template.png name to conform with macOS dark theme
+    const trayIconPath = path.join(assetsDirectory, trayIcon)
+    const tray = new remote.Tray(trayIconPath)
+
+    const trayUpdateInterval = 60 * 1000
+    const updateTray = () => {
+      const now = moment()
+      const elapsed = now.diff(stopWatch)
+      tray.setTitle(moment.duration(elapsed, "milliseconds").humanize())
+    }
+    updateTray()
+    setInterval(updateTray, trayUpdateInterval)
+
+    const contextMenu = remote.Menu.buildFromTemplate([
+      {label: "About", click() {
+        remote.dialog.showMessageBox(
+          { message: "Simple Clock and Stop Watch",
+            buttons: ["OK"] })
+      }},
+      {label: "Reset Stop Watch", click() {
+        resetStopWatch()
+        updateTray()
+      }},
+      {label: "Quit", click() {
+        // remote.app.quit()
+        // alert("quit")
+        ipcRenderer.send("quit")
+      }},
+    ])
+    tray.setToolTip("Simple Clock and Stop Watch")
+    tray.setContextMenu(contextMenu)
+}
+
+startupTray()
+
+$(".wrapper").hide()
+fadeIn()
+
+ipcRenderer.on("fadeIn", fadeIn)
+ipcRenderer.on("fadeOut", fadeOut)
